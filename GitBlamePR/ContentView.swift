e2b41ca0d70ddaede1aa3a8a45943e14a5e27ec4
aoyama <m@aoyama.dev>
2019-12-07 16:16:30 +0900

    Initial Commit

---
+++ GitBlamePR/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GitBlamePR
+//
+//  Created by Makoto Aoyama on 2019/12/07.
+//  Copyright © 2019 dev.aoyama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
