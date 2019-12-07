a39c4e516a730bcd535faaee66da6d40d04cbebd
Makoto Aoyama <m@aoyama.dev>
2019-12-08 00:39:37 +0900

    Add open github command

--- EditorExtension/OpenGitHubCommand.swift
+++ EditorExtension/OpenGitHubCommand.swift
@@ -1,5 +1,5 @@
 //
-//  SourceEditorCommand.swift
+//  OpenGitHubCommand.swift
 //  EditorExtension
 //
 //  Created by Makoto Aoyama on 2019/12/07.
@@ -9,11 +9,19 @@
 import Foundation
 import XcodeKit
 
-class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+class OpenGitHubCommand: NSObject, XCSourceEditorCommand {
     
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
         // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
-        
+
+        // Retrieve the contents of the current source editor.
+        let lines = invocation.buffer.lines
+        // Reverse the order of the lines in a copy.
+        let updatedText = Array(lines.reversed())
+        lines.removeAllObjects()
+        lines.addObjects(from: updatedText)
+        // Signal to Xcode that the command has completed.
+
         completionHandler(nil)
     }
     
