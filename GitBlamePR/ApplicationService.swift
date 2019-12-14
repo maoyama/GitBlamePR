44212de37bcc93c3aad4aaada5a322881dc2fcf7
Makoto Aoyama <m@aoyama.dev>
2019-12-15 00:29:24 +0900

    Add content view model initializer for stdout

--- GitBlamePR/ApplicationService.swift
+++ GitBlamePR/ApplicationService.swift
@@ -43,3 +43,36 @@ class ApplicationService {
 
 
 }
+
+extension ContentViewModel {
+    init?(gitRemoteStandardOutput: String, gitBlamePRStandardOutput: String) {
+        guard let repo = gitRemoteStandardOutput
+            .components(separatedBy: .newlines)[0]
+            .components(separatedBy: "github.com:")
+            .last?
+            .components(separatedBy: ".git")
+            .first else { return nil }
+        let repoURL = "https://github.com/" + repo
+
+        let strLines = gitBlamePRStandardOutput.components(separatedBy: .newlines)
+        self.lines = strLines.map { (line) -> (message: String, url: URL, code: String) in
+            let linePrefix = line.prefix(9)
+            let code = line.suffix(line.count - linePrefix.count)
+            let trimdlinePrefix = linePrefix.trimmingCharacters(in: .whitespacesAndNewlines)
+            let message = trimdlinePrefix.replacingOccurrences(of: "^", with: "")
+            return (
+                message: message,
+                url: Self.url(message: message, repoURL: repoURL),
+                code: String(code)
+            )
+        }
+    }
+
+    private static func url(message: String, repoURL: String) -> URL {
+        if message.contains("PR") {
+            let prNumber = message.components(separatedBy: "#").last!
+            return URL(string: repoURL + "/pull/" + prNumber)!
+        }
+        return URL(string: repoURL + "/commit/" + message)!
+    }
+}
