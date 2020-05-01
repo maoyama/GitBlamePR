efbef8e64cd849b63d278033971077e4cae6876c
Makoto Aoyama <m@aoyama.dev>
2020-05-02 00:40:14 +0900

    Add section title view

---
+++ GitBlamePR/View/SectionTitleView.swift
@@ -0,0 +1,21 @@
+//
+//  SectionTitleView.swift
+//  GitBlamePR
+//
+//  Created by Makoto Aoyama on 2020/05/02.
+//  Copyright © 2020 dev.aoyama. All rights reserved.
+//
+
+import SwiftUI
+
+struct SectionTitleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SectionTitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        SectionTitleView()
+    }
+}
