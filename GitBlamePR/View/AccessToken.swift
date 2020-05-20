7f664785ade51201b88be68385f13bcb0a9111b2
Makoto Aoyama <m@aoyama.dev>
2020-05-20 19:15:50 +0900

    Add access token view

---
+++ GitBlamePR/View/AccessToken.swift
@@ -0,0 +1,53 @@
+//
+//  AccessToken.swift
+//  GitBlamePR
+//
+//  Created by Makoto Aoyama on 2020/05/20.
+//  Copyright © 2020 dev.aoyama. All rights reserved.
+//
+
+import SwiftUI
+
+struct AccessTokenView: View {
+    @State var token: String = ""
+    var hasToken: Bool
+    var body: some View {
+        VStack {
+            Text("Personal access token")
+                .fontWeight(.bold)
+            Text("""
+                Token are needed to get a Pull Request for the following reasons:
+                - To avoid api limit
+                - To access private repositoies
+                """)
+            if hasToken {
+                HStack {
+                    Text("There are previously saved token.")
+                        .foregroundColor(.secondary)
+                    Button("Delete Token") {
+                        print(self.token)
+                    }
+                }
+            } else {
+                HStack {
+                    SecureField("", text: $token)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(minWidth: 300)
+                    Button("Save") {
+                        print(self.token)
+                    }
+                }
+            }
+        }
+            .padding()
+    }
+}
+
+struct AccessToken_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            AccessTokenView(hasToken: false)
+            AccessTokenView(hasToken: true)
+        }
+    }
+}
---
+++ GitBlamePR/View/DetailFooter.swift
@@ -0,0 +1,36 @@
+//
+//  DetailFooter.swift
+//  GitBlamePR
+//
+//  Created by Makoto Aoyama on 2020/05/19.
+//  Copyright © 2020 dev.aoyama. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailFooter: View {
+    @State var show = false
+    var body: some View {
+        VStack {
+            Divider()
+            Text("Personal access token")
+                .onTapGesture {
+                    self.show = true
+                }
+
+        }
+            .popover(isPresented: $show) {
+                AccessTokenView(hasToken: false)
+            }
+
+    }
+}
+
+struct DetailFooter_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            DetailFooter()
+        }
+    }
+}
+
