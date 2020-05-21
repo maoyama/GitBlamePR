93b77527b14ab2b269e05a8d1ec8c5f815687f16
Makoto Aoyama <m@aoyama.dev>
2020-05-21 10:02:46 +0900

    Add detail footer styling

--- GitBlamePR/View/DetailFooter.swift
+++ GitBlamePR/View/DetailFooter.swift
@@ -11,18 +11,24 @@ import SwiftUI
 struct DetailFooter: View {
     @State var show = false
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
             Divider()
-            Text("Personal access token")
-                .onTapGesture {
-                    self.show = true
+            HStack {
+                Image("GitHub-Mark-S")
+                    .resizable()
+                    .renderingMode(.original)
+                    .frame(width: 20, height: 20)
+                Text("Personal access token")
+                    .onTapGesture {
+                        self.show = true
                 }
-
+            }
+                .padding(8)
         }
             .popover(isPresented: $show) {
                 PersonalAccessTokenViewWrapper()
             }
-
+            .background(Color(.windowBackgroundColor))
     }
 }
 
@@ -30,6 +36,9 @@ struct DetailFooter_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             DetailFooter()
+                .environment(\.colorScheme, .light)
+            DetailFooter()
+                .environment(\.colorScheme, .dark)
         }
     }
 }
